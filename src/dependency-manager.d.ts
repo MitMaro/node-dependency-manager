52c0a4a1e396e9248b3af389cbc66c723a367476
Tim Oram <dev@mitmaro.ca>
2018-02-11 00:26:58 -0330

    Add API documentation

--- src/dependency-manager.d.ts
+++ src/dependency-manager.d.ts
@@ -2,15 +2,26 @@
 // Project: @mitmaro/dependency-manager
 // Definitions by: Tim Oram <https://github.com/MitMaro>
 
+interface IOptions {
+	/** Toggle the use of snake case in identifiers */
+	snakeCase?: boolean;
+	/** Toggle the use of argument injection */
+	argumentInjection?: boolean;
+}
+
 export class DependencyManager {
-	/** Create an instance of the dependency manager */
-	public constructor();
+	/**
+	 * Create an instance of the dependency manager
+	 * @param options
+	 */
+	public constructor(options?: IOptions);
 
 	/**
 	 * Register a dependency
 	 * @param name The name of the dependency
 	 * @param factory The factory function that will create the instance
 	 * @param dependencyNames An array of dependency names
+	 * @throws {Error} if there is a invalid dependency
 	 */
 	public register<T, D>(name: string, factory: (deps: D) => T, dependencyNames?: string[]): void;
 
@@ -18,6 +29,7 @@ export class DependencyManager {
 	 * Set a static instance as a dependency
 	 * @param name The name of the dependency
 	 * @param instance The instance of the dependency
+	 * @throws {Error} if there is a invalid dependency
 	 */
 	public set<T>(name: string, instance: T): void;
 
@@ -25,6 +37,7 @@ export class DependencyManager {
 	 * Add an alias name to a dependency name
 	 * @param name The name of the dependency
 	 * @param alias The alias name
+	 * @throws {Error} if there is a invalid dependency
 	 */
 	public alias(name: string, alias: string): void;
 
@@ -38,6 +51,7 @@ export class DependencyManager {
 
 	/**
 	 * Load all registered dependencies
+	 * @throws {Error} if a dependencies does not exist or cannot be loaded
 	 */
 	public load(): Promise<void>;
 }
